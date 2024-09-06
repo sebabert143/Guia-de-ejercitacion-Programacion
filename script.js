ba60e331abfc98c8681649a996007136d1f66fd5
sebabert143 <sebastianbertoluzza@gmail.com>
2024-09-06 11:50:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ function validateForm() {
     }
 
     // Si todas las validaciones pasan, se envía el formulario
-    window.location.href = "https://www.aerolineas.com.ar/";
+    window.location.href = "https://www.youtube.com/";
     return true;
 }
